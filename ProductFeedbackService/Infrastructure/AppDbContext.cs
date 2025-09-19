b03c271c80493d0c03434adf97ddcb1c06d29d33
Oleksandr Chornenkyi <oleksandr.chornenkyi10@gmail.com>
2025-09-19 06:06:48 +0300

    feat: add AppDbContext

---
+++ ProductFeedbackService/Infrastructure/AppDbContext.cs
@@ -0,0 +1,14 @@
+// Infrastructure/AppDbContext.cs
+namespace ProductFeedbackService.Infrastructure;
+
+using Microsoft.EntityFrameworkCore;
+using ProductFeedbackService.Domain.Models;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+    public DbSet<Product> Products => Set<Product>();
+    public DbSet<Feedback> Feedbacks => Set<Feedback>();
+    public DbSet<WordRating> WordRatings => Set<WordRating>();
+    public DbSet<ProductRating> ProductRatings => Set<ProductRating>();
+}
--- ProductFeedbackService/Program.cs
+++ ProductFeedbackService/Program.cs
@@ -1,16 +1,33 @@
-using ProductFeedbackService.Infrastructure.Storage;
+using ProductFeedbackService.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using ProductFeedbackService.Domain.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddSingleton<FeedbackStorage>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("InMemoryDb"));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    if (!db.WordRatings.Any())
+    {
+        db.WordRatings.AddRange(
+            new WordRating { Phrase = "excellent", Score = 5 },
+            new WordRating { Phrase = "good", Score = 4 },
+            new WordRating { Phrase = "average", Score = 3 },
+            new WordRating { Phrase = "bad", Score = 2 },
+            new WordRating { Phrase = "very bad", Score = 1 }
+        );
+        db.SaveChanges();
+    }
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -18,9 +35,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
